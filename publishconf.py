fc0db3935510c5c618e7d555271fe9d21e3d21e2
Rémy HUBSCHER <hubscher.remy@gmail.com>
2020-09-20 23:25:20 +0200

    Fix atom config.

--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,6 @@ SITEURL = 'https://sea.hubscher.info/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
